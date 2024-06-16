67147d1acb3357c766e8621fe3f314fa7fe2ced5
katheer <[email protected]>
2024-06-16 05:01:18 +0300

    token

--- lib/pages/core/api/api_interceptors.dart
+++ lib/pages/core/api/api_interceptors.dart
@@ -4,11 +4,15 @@ import 'package:flutter_application_1/pages/core/api/end_ponits.dart';
 
 class ApiInterceptor extends Interceptor {
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    // options.headers[ApiKey.token] =
+    //     CacheHelper().getData(key: ApiKey.token) != null
+    //         ? '${CacheHelper().getData(key: ApiKey.token)}'
+    //         : null;
     var token = await CacheHelper().getData(key: ApiKey.token);
-    if (token != null) {
-      options.headers['Authorization'] = 'Bearer $token';
-    }
+    options.headers['Authorization'] = token != null ? 'Bearer $token' : null;
+
     super.onRequest(options, handler);
   }
 }
